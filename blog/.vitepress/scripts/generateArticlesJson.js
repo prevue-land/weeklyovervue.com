6d35ec905c4f71c5dd46fd4e90aaa19f208ca279
Maciej Pędzich <dnalop.me@gmail.com>
2022-02-10 19:35:31 +0100

    Create a script for generating articles.json

---
+++ blog/.vitepress/scripts/generateArticlesJson.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const path = require('path');
+const frontmatter = require('front-matter');
+
+const articlesDir = path.resolve(__dirname, '../../articles');
+const articleFiles = fs.readdirSync(articlesDir);
+
+const data = [];
+
+for (const file of articleFiles) {
+  const articleFilePath = path.join(articlesDir, `/${file}`);
+  const fileContent = fs.readFileSync(articleFilePath, {
+    encoding: 'utf-8'
+  });
+
+  const { attributes } = frontmatter(fileContent);
+  const { title, date, description, cover } = attributes;
+
+  const [slug] = file.split('.');
+  const link = `/articles/${slug}.html`;
+
+  data.push({
+    title,
+    description,
+    link,
+    date: new Date(date).getTime(),
+    cover: cover.replace('blog/public', 'images')
+  });
+}
+
+data.sort((a, b) => b.date - a.date);
+
+const articlesJson = JSON.stringify(data);
+
+fs.writeFileSync(path.resolve(__dirname, '../../articles.json'), articlesJson);
+console.log('articles.json generated successfully');
+process.exit();
