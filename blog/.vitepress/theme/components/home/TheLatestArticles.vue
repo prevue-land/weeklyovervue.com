65eb40dd7289fdd325f594488ca6fa1d9b8a9028
Maciej Pędzich <dnalop.me@gmail.com>
2022-03-02 20:48:17 +0100

    Comment out components using articles.json

--- blog/.vitepress/theme/components/home/TheLatestArticles.vue
+++ blog/.vitepress/theme/components/home/TheLatestArticles.vue
@@ -1,15 +1,15 @@
 <script setup>
-import articles from '../../../../articles.json';
+// import articles from '../../../../articles.json';
 
-const data = articles.slice(0, 3);
+// const data = articles.slice(0, 3);
 </script>
 
 <template>
   <section id="the-latest-articles" class="home-section">
     <h1>The latest articles</h1>
-    <div class="articles-grid">
+    <!-- <div class="articles-grid">
       <ArticleCard v-for="article in data" :key="article.id" v-bind="article" />
-    </div>
+    </div> -->
   </section>
 </template>
 
--- blog/.vitepress/theme/index.js
+++ blog/.vitepress/theme/index.js
@@ -3,19 +3,19 @@ import DefaultTheme from 'vitepress/theme';
 import Layout from './Layout.vue';
 import './custom.css';
 
-import ArticleCard from './components/ArticleCard.vue';
-import AboutSection from './components/home/AboutSection.vue';
-import TheLatestArticles from './components/home/TheLatestArticles.vue';
-import NewsletterSignup from './components/home/NewsletterSignup.vue';
+// import ArticleCard from './components/ArticleCard.vue';
+// import AboutSection from './components/home/AboutSection.vue';
+// import TheLatestArticles from './components/home/TheLatestArticles.vue';
+// import NewsletterSignup from './components/home/NewsletterSignup.vue';
 
 export default {
   ...DefaultTheme,
   Layout,
   NotFound: () => 'Looks like this page does not exist',
   enhanceApp({ app }) {
-    app.component('ArticleCard', ArticleCard);
-    app.component('AboutSection', AboutSection);
-    app.component('TheLatestArticles', TheLatestArticles);
-    app.component('NewsletterSignup', NewsletterSignup);
+    // app.component('ArticleCard', ArticleCard);
+    // app.component('AboutSection', AboutSection);
+    // app.component('TheLatestArticles', TheLatestArticles);
+    // app.component('NewsletterSignup', NewsletterSignup);
   }
 };
